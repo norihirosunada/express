36e9e88a00aa1745e863d42028c6f241ce40c182
norihirosunada <norisuna1201@gmail.com>
2020-02-08 17:36:50 +0000

    upload to heroku

--- Gemfile
+++ Gemfile
@@ -12,5 +12,12 @@ gem 'sinatra-contrib'
 
 gem 'sinatra-activerecord'
 gem 'activerecord', '5.2.3'
-gem 'sqlite3', '1.4.1'
 gem 'rake'
+
+group :production do
+    gem 'pg', '~> 0.21.0'
+end
+
+group :development do
+    gem 'sqlite3', '1.4.1'
+end
--- models.rb
+++ models.rb
@@ -1,4 +1,9 @@
-ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
+require 'bundle/setup'
+Bundler.require
+
+if development?
+    ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
+end
 
 class History < ActiveRecord::Base
 end
